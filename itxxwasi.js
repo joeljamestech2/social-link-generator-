133ef600a75e2d270b9315e81632244cc431e681
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-10-08 12:39:27 +0300

    Initial commit

---
+++ itxxwasi.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let server = require('./wasiqr'),
+    code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/wasiqr', server);
+app.use('/code', code);
+app.use('/whats',async (req, res, next) => {
+res.sendFile(__path + '/Whatsapp.html')
+})
+app.use('/facebook',async (req, res, next) => {
+res.sendFile(__path + '/facebook.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/joelpage.html')
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`
+link generator by lord joel
+
+ Server running on http://localhost:` + PORT)
+})
+
+module.exports = app
+/**
+    powered by wasi tech team 
+    join Whatsapp channel for more updates 
+    **/
